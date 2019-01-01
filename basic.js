4b7657cf06e0b039f1628fea872d6ce7724681c6
Mahamat Legrand <legrandmahamat1@gmail.com>
2019-01-01 21:13:53 +0100

    100DaysOfCode Initial commit

---
+++ basic.js
@@ -0,0 +1,103 @@
+/* Day #1 100DaysOfCode - TUESDAY 1 JANUARY 2019 */
+
+//Javascript Comment
+
+/*Javascript Multi-line comment
+ */
+
+//Basic Javascript:Declare Javascript Variables
+var myName;
+
+//Basic Javascript:Storing Values wuth the Assignment Operator
+var a;
+var b = 2;
+
+var a = 7;
+var b = a;
+
+//Basic Javascript:Initializing Variables with the Assignment Operator
+var a = 9;
+
+//Basic Javascript:Understanding Uninitialized Variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+//Basic Javascript:Understanding Case Sensitivity in Variables
+//camelCase
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+//Basic Javascript:Add Two Numbers with Javascript
+var sum = 10 + 10;
+
+//Basic Javascript:Subtract One Number from Another with JavaScript
+var difference = 45 - 33;
+
+//Basic Javascript:Multiply Two Numbers with Javascript
+var product = 8 * 10;
+
+//Basic Javascript:Divide One Number by Anothe with Javascript
+var division = 66 / 33;
+
+//Basic Javascript:Increment a Number with Javascript
+var inc = 3;
+inc++;
+
+//Basic Javascript:Decrement a Number with Javascript
+var dec = 4;
+dec--;
+
+//Basic Javascript:Create Decimal Numbers with Javascript
+var myDecimal = 8.5;
+
+//Basic Javascript:Multiply Two Decimals with Javascript
+var product = 2.0 * 2.5;
+
+//Basic Javascript:Divide One Decimal by Another with Javascript
+var quotient = 4.4 / 2.0;
+
+//Basic Javascript:Finding a Remainder in Javascript
+var remainder = 11 % 3;
+
+//Basic Javascript:Compound Assignment With Augmented Addition
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+//Basic Javascript:Compound Assignment With Augmented Subtraction
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+//Basic Javascript:Compound Assignment With Augmented Multiplication
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+//Basic Javascript:Compound Assignment With Augmented Division
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+console.log(a);
+
+/* Day #2 100DaysOfCode */
+
+//Basic Javascript:Declare String Variables
