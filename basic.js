6e379f2ef8bc0b9955c1842086ac2c6d408e3026
Mahamat Legrand <legrandmahamat1@gmail.com>
2019-01-13 18:17:23 +0100

    Day #13 100DaysOfCode & 301DaysOfCode

--- basic.js
+++ basic.js
@@ -897,3 +897,62 @@ function updateRecords(id, prop, value) {
 /* Day #13 100DaysOfCode & 301DaysOfCode*/
 
 //Basic Javascript:Iterate with JavaScript While Loops
+var myArray = [];
+var i = 0;
+while (i < 5) {
+  myArray.push(i);
+  i++;
+}
+
+//Basic Javascript:Iterate with JavaScript For Loops
+var myArray = [];
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
+
+//Basic Javascript:Iterate Odd Numbers With a For Loops
+var myArray = [];
+for (var i = 1; i < 11; i += 2) {
+  myArray.push(i);
+}
+
+//Basic Javascript:Count Backwards With a For Loop
+var myArray = [];
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
+
+//Basic Javascript:Iterate Through an Array with a For Loop
+var myArr = [2, 3, 4, 5, 6];
+var total = 0;
+for (var i = 0; i < myArr.length; i++) {
+  total += myArr[i];
+  // console.log(total);
+}
+
+//Basic Javascript:Nesting For Loops
+function multiplyAll(arr) {
+  var product = 1;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+  return product;
+}
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+//Basic Javascript:Iterate with JavaScript Do...While Loops
+var myArray = [];
+var i = 10;
+
+do {
+  myArray.push(i);
+  i++;
+} while (i < 10);
+
+/* Day #14 100DaysOfCode & 301DaysOfCode*/
+
+//Basic Javascript:Profile Lookup
+
+// console.log(myArray);
